d5bc54768e0a00ebd894dfbfdbaa2c755f7e91b3
Kalle <kalle@utiligize.com>
2020-09-14 15:35:43 +0200

    Manual beta_2

--- R/beta_2.R
+++ R/beta_2.R
@@ -1,8 +1,9 @@
 #' @title Forecast Ageing Rate
 #' @description This function calculates the forecast Ageing Rate for an
 #' electric network asset. See section 6.1.8 on page 34 in CNAIM (2017).
-#' @param expected_life_years Numeric. The output returned by the
-#' function \code{\link{expected_life}}().
+#' @param current_health_score Numeric. The output returned by the
+#' function \code{\link{current_health}}().
+#' @inheritParams pof_future_transformer_11kv
 #' @return Numeric. Forecast ageing rate for an electric network asset.
 #' @source DNO Common Network Asset Indices Methodology (CNAIM),
 #' Health & Criticality - Version 1.1, 2017:
