a54b6bde5564b6f348a864d1862e4db9f6ebaa2a
Kalle <kalle@utiligize.com>
2020-09-25 14:18:15 +0200

    Test overhead lines

---
+++ tests/testthat/test-pof_future_ohl_cond_132_66_33kv.R
@@ -0,0 +1,26 @@
+library(testthat)
+library(CNAIM)
+
+context("Future Probability of Failure for 66kV OHL (Tower Line) Conductor")
+
+test_that("pof_future_ohl_cond_132_66_33kv", {
+  # TODO: verify correctness
+
+  res <- pof_future_ohl_cond_132_66_33kv(ohl_conductor = "66kV OHL (Tower Line) Conductor",
+                                  sub_division = "Cu",
+                                  placement = "Default",
+                                  altitude_m = 130,
+                                  distance_from_coast_km = 1,
+                                  corrosion_category_index = 1,
+                                  age = 12,
+                                  conductor_samp = "Medium/Normal",
+                                  corr_mon_survey = "Medium/Normal",
+                                  visual_cond = "Normal Wear",
+                                  midspan_joints = 3,
+                                  reliability_factor = "Default",
+                                  simulation_end_year = 100)
+
+  expect_equal(res, 0.004837129)
+
+})
+
