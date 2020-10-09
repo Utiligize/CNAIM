7e7060940d8a87421af28304405de69a37b8abab
mozin <mohsinvindhani@gmail.com>
2020-10-09 20:37:37 +0530

    cof init

---
+++ tests/testthat/test-cof_hv_switchgear_distribution.R
@@ -0,0 +1,49 @@
+library(testthat)
+library(CNAIM)
+
+context("COF:Financial HV switch gear distribution")
+
+test_that("6.6/11kV CB (GM) Secondary", {
+  res <- financial_cof_hv_switchgear_distribution(hv_asset_category = "6.6/11kV CB (GM) Secondary",
+                                               access_factor_criteria = "Type A")
+
+  expected_value <- 5792
+
+  expect_equal(res, expected_value)
+})
+
+
+context("COF:Safety HV switch gear distribution")
+
+test_that("6.6/11kV CB (GM) Secondary", {
+  res <- safety_cof_hv_switchgear_distribution(hv_asset_category = "6.6/11kV CB (GM) Secondary",
+                                            location_risk = "Default",
+                                            type_risk = "Default")
+  expected_value <- 4262
+
+  expect_equal(res, expected_value)
+})
+
+
+context("COF:Environmental HV switch gear distribution")
+
+test_that("6.6/11kV CB (GM) Secondary", {
+  res <- environmental_cof_hv_switchgear_distribution(hv_asset_category = "6.6/11kV CB (GM) Secondary",
+                                                      type_env_factor = "Oil",
+                                                      prox_water = 95,
+                                                      bunded = "Yes")
+  expected_value <- 537.38
+
+  expect_equal(res, expected_value)
+})
+
+
+context("COF:Network HV switch gear distribution")
+
+test_that("6.6/11kV CB (GM) Secondary", {
+  res <- network_cof_hv_switchgear_distribution(hv_asset_category = "6.6/11kV CB (GM) Secondary",
+                                             no_customers = 750, kva_per_customer = 51) %>% round(1)
+  expected_value <- 182343.8
+
+  expect_equal(res, expected_value)
+})
