eca293bf3c2ad94785887b5c6ccff8c67d625424
mozin <mohsinvindhani@gmail.com>
2022-06-15 20:35:38 +0530

    gsub in cof lv switchgear

--- R/cof_lv_switchgear_and_other.R
+++ R/cof_lv_switchgear_and_other.R
@@ -235,8 +235,7 @@ network_cof_lv_switchgear_and_other <- function(lv_asset_category,
     }
   }
 
-  adj_cust_no <-
-    adj_cust_no %>% stringr::str_match_all("[0-9]+") %>% unlist %>% as.numeric
+  adj_cust_no <- gsub("([0-9]+).*$", "\\1", adj_cust_no) %>% as.numeric()
 
   customer_factor <- (adj_cust_no * no_customers) / ref_no_cust
 
