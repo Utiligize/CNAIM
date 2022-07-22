11e061868e77b428f6792a01a462b874640593da
mozin <mohsinvindhani@gmail.com>
2022-07-22 23:39:29 +0530

    30 60 KV init

--- R/pof_transformer_30_60kv.R
+++ R/pof_transformer_30_60kv.R
@@ -106,56 +106,53 @@
 #' @inheritParams oil_test_modifier
 #' @inheritParams dga_test_modifier
 #' @inheritParams ffa_test_modifier
-#' @return Numeric. Current probability of failure.
+#' @return DataFrame Current probability of failure
+#' per annum per kilometer along with current health score.
 #' @export
 #' @examples
 #' # Current probability of failure for a 60/10kV transformer
-# pof_transformer_30_60kv_res <-
-# pof_transformer_30_60kv(transformer_type = "60kV Transformer (GM)",
-# year_of_manufacture = 1980,
-# utilisation_pct = "Default",
-# no_taps = "Default",
-# placement = "Default",
-# altitude_m = "Default",
-# distance_from_coast_km = "Default",
-# corrosion_category_index = "Default",
-# age_tf = 43,
-# age_tc = 43,
-# partial_discharge_tf = "Default",
-# partial_discharge_tc = "Default",
-# temperature_reading = "Default",
-# main_tank = "Default",
-# coolers_radiator = "Default",
-# bushings = "Default",
-# kiosk = "Default",
-# cable_boxes = "Default",
-# external_tap = "Default",
-# internal_tap = "Default",
-# mechnism_cond = "Default",
-# diverter_contacts = "Default",
-# diverter_braids = "Default",
-# moisture = "Default",
-# acidity = "Default",
-# bd_strength = "Default",
-# hydrogen = "Default",
-# methane = "Default",
-# ethylene = "Default",
-# ethane = "Default",
-# acetylene = "Default",
-# hydrogen_pre = "Default",
-# methane_pre = "Default",
-# ethylene_pre = "Default",
-# ethane_pre = "Default",
-# acetylene_pre = "Default",
-# furfuraldehyde = "Default",
-# reliability_factor = "Default",
-# k_value = 0.454,
-# c_value = 1.087,
-# normal_expected_life_tf = "Default",
-# normal_expected_life_tc = "Default") * 100
-# paste0(sprintf("Probability of failure %.4f", pof_transformer_30_60kv_res),
-# " percent per annum")
-
+#' pof_transformer_30_60kv(transformer_type = "60kV Transformer (GM)",
+#' year_of_manufacture = 1980,
+#' utilisation_pct = "Default",
+#' no_taps = "Default",
+#' placement = "Default",
+#' altitude_m = "Default",
+#' distance_from_coast_km = "Default",
+#' corrosion_category_index = "Default",
+#' age_tf = 43,
+#' age_tc = 43,
+#' partial_discharge_tf = "Default",
+#' partial_discharge_tc = "Default",
+#' temperature_reading = "Default",
+#' main_tank = "Default",
+#' coolers_radiator = "Default",
+#' bushings = "Default",
+#' kiosk = "Default",
+#' cable_boxes = "Default",
+#' external_tap = "Default",
+#' internal_tap = "Default",
+#' mechnism_cond = "Default",
+#' diverter_contacts = "Default",
+#' diverter_braids = "Default",
+#' moisture = "Default",
+#' acidity = "Default",
+#' bd_strength = "Default",
+#' hydrogen = "Default",
+#' methane = "Default",
+#' ethylene = "Default",
+#' ethane = "Default",
+#' acetylene = "Default",
+#' hydrogen_pre = "Default",
+#' methane_pre = "Default",
+#' ethylene_pre = "Default",
+#' ethane_pre = "Default",
+#' acetylene_pre = "Default",
+#' furfuraldehyde = "Default",
+#' reliability_factor = "Default",
+#' k_value = 0.454,
+#' c_value = 1.087,
+#' normal_expected_life_tf = "Default",
+#' normal_expected_life_tc = "Default")
 pof_transformer_30_60kv <- function(transformer_type = "60kV Transformer (GM)",
                                     year_of_manufacture,
                                     utilisation_pct = "Default",
@@ -920,5 +917,5 @@ pof_transformer_30_60kv <- function(transformer_type = "60kV Transformer (GM)",
        (((c * current_health_score)^2) / factorial(2)) +
        (((c * current_health_score)^3) / factorial(3)))
 
-  return(probability_of_failure)
+  return(data.frame(pof = probability_of_failure, chs = current_health_score))
 }
