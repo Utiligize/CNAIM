bf19c62c84c1f91dc1c683617d6e795cbed4cc18
mozin <mohsinvindhani@gmail.com>
2022-07-25 21:52:44 +0530

    33 66 kv init

--- tests/testthat/test-pof_transformer_33_66kv.R
+++ tests/testthat/test-pof_transformer_33_66kv.R
@@ -4,10 +4,15 @@ library(CNAIM)
 context("Current Probability of Failure for a 66kV Transformer (GM)")
 
 test_that("pof_transformer_33_66kv", {
-  expect_equal(pof_transformer_33_66kv(transformer_type = "66kV Transformer (GM)",
+  res <- pof_transformer_33_66kv(transformer_type = "66kV Transformer (GM)",
                                     year_of_manufacture = 1980,
                                     age_tf=43,
-                                    age_tc=43), 0.01244618)
+                                    age_tc=43) %>% round(6)
+
+  expected_val <- data.frame(pof = 0.012446, chs = 3.931585)
+
+  expect_equal(res, expected_val)
+
 })
 
 
