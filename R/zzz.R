5a334b53014ff0e2a9adf5193cd13b74cdb7827a
KennethRR <38689879+KennethRR@users.noreply.github.com>
2022-08-23 14:48:03 +0200

    Updated citation to latest version of package

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("The CNAIM package is created by Utiligize.
-Please cite the CNAIM package as: Utiligize (2020). CNAIM: R package version 1.0.0, www.cnaim.io.")
+Please cite the CNAIM package as: Utiligize (2022). CNAIM: R package version 2.1.2, www.cnaim.io.")
 }
