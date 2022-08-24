596437b76471c82c843d9777a9d64feb8772e533
KennethRR <38689879+KennethRR@users.noreply.github.com>
2022-08-24 10:42:56 +0200

    Update risk_calculation.R
    Fixed a calculation error. Changed names of input variables to better reflect the standard.

--- R/risk_calculation.R
+++ R/risk_calculation.R
@@ -1,55 +1,81 @@
 #' @title Calculates risk and converts to matrix coordinates
-#' @description This function calculates monetary risk, given probability of
-#' failure and consequence of failure inputs, as well as the desired risk matrix
+#' @description This function calculates risk matrix coordinates
 #' dimensions.
 #' @param matrix_dimensions A data frame with the dimensions of the desired risk
-#' matrix
-#' @param id A string that describes the asset
-#' @param pof The probability of failure of the asset
-#' @param cof The consequences of failure of the asset
+#' matrix.
+#' @param id An integer that identifies the asset
+#' @param chs The Current Health Score (CHS) of the asset
+#' @param cof The Consequence of Failure of the asset
 #' @param asset_type The asset type to be calculated for class
-#' @param pof_limits Specific limit for the risk matrix
-#' @param cof_limits Specific limit for the risk matrix
+#' @param hi_bands Specific Health Index (HI) bands for risk matrix. Default
+#' values are the same as defined in the CNAIM v2.1 standard
+#' @param ci_bands Specific Criticality Index (CI) bands for the risk matrix.
+#' Default values are the same as defined in the CNAIM v.2.1 standard.
 #' @param asset_type The asset type to be calculated for class
+#' @source DNO Common Network Asset Indices Methodology (CNAIM),
+#' Health & Criticality - Version 2.1, 2021:
+#' \url{https://www.ofgem.gov.uk/sites/default/files/docs/2021/04/dno_common_network_asset_indices_methodology_v2.1_final_01-04-2021.pdf}
 #' @export
+#' @examples
+#' # Calculate risk matrix coordinates for an asset
+#' # 1. Make the risk matrix structure
+#' matrix_structure <- risk_matrix_structure(5,4,NA)
 #'
-risk_calculation <- function(matrix_dimensions,id,pof,cof,asset_type, pof_limits, cof_limits){
+#' # 2. Calculate risk matrix coordinates
+#' risk_calculation(matrix_dimensions = matrix_structure,
+#' id = 1,
+#' chs = 4,
+#' cof = 15000,
+#' asset_type = "6.6/11kV Transformer (GM)")
+#'
+#'
+
+risk_calculation <- function(matrix_dimensions,id,chs,cof,asset_type,
+                             hi_bands = NULL, ci_bands = NULL){
 
   `Asset Register Category` = `Total - (GBP)` = NULL
   # due to NSE notes in R CMD check
 
-  reference_cof <- gb_ref$reference_costs_of_failure %>%
+  reference_cof <- CNAIM:::gb_ref$reference_costs_of_failure %>%
     dplyr::filter(`Asset Register Category` == asset_type) %>%
     dplyr::select(`Total - (GBP)`) %>%
     dplyr::pull()
 
-  for (i in 1:length(pof_limits)) {
+  if (is.null(hi_bands)){hi_bands <- c(3, 5, 5.5, 6.5, 8, 15)}
+  if (is.null(ci_bands)){ci_bands <- c(75, 125, 200, Inf)}
+  ci <- (cof/reference_cof)*100
 
-    if(pof < pof_limits[i] && i==1) {
-      pof_pct <- ((pof - 0.5) / (pof_limits[i]-0.5)) * (100/length(pof_limits))
+  for (i in 1:length(hi_bands)) {
+
+    if(chs < hi_bands[i] && i==1) {
+      pof_pct <- ((chs - 0.5) / (hi_bands[i]-0.5)) * (100/length(hi_bands))
       break
 
-    } else if (pof < pof_limits[i] ) {
-      pof_pct <- ((pof - pof_limits[i-1]) / (pof_limits[i]-pof_limits[i-1])) * (100/length(pof_limits)) + (100/length(pof_limits))*(i-1)
+    } else if (chs < hi_bands[i] ) {
+      pof_pct <- ((chs - hi_bands[i-1]) / (hi_bands[i]-hi_bands[i-1])) *
+        (100/length(hi_bands)) + (100/length(hi_bands))*(i-1)
       break
 
-    } else if (pof > 15)  {
+    } else if (chs > 15)  {
       pof_pct <- 100
     }
 
   }
 
-  for (i in 1:length(cof_limits)) {
 
-    if(cof < cof_limits[i] && i==1) {
-      cof_pct <- ((cof - 0) / (cof_limits[i]- 0)) * (100/length(cof_limits))
+
+  for (i in 1:length(ci_bands)) {
+
+    if(ci < ci_bands[i] && i==1) {
+      cof_pct <- ((ci - 0) / (ci_bands[i]- 0)) * (100/length(ci_bands))
       break
 
-    } else if (cof < cof_limits[i] ) {
-      cof_pct <- ((cof - cof_limits[i-1]) / (cof_limits[i]-cof_limits[i-1])) * (100/length(cof_limits)) + (100/length(cof_limits))*(i-1)
+    } else if (ci < ci_bands[i] ) {
+      cof_pct <- ((ci - ci_bands[i-1]) / (ci_bands[i]-ci_bands[i-1])) *
+        (100/length(ci_bands)) + (100/length(ci_bands))*(i-1)
       break
 
-    } else if (cof > 250)  {
+    } else if (ci > 250)  {
       cof_pct <- 100
     }
 
