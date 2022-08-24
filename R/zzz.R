ffdc6418ba12a69821d102defd7ee97de350dbc1
mozin <mohsinvindhani@gmail.com>
2022-08-24 21:54:56 +0530

    Pacage versin update

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("The CNAIM package is created by Utiligize.
-Please cite the CNAIM package as: Utiligize (2022). CNAIM: R package version 2.1.2, www.cnaim.io.")
+Please cite the CNAIM package as: Utiligize (2022). CNAIM: R package version 2.1.3, www.cnaim.io.")
 }
