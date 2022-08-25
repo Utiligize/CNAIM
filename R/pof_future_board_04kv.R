3a988d6d1b3ad79defea9f52c7d1fa275107ece1
mozin <mohsinvindhani@gmail.com>
2022-08-25 10:33:05 +0530

    PoF Future board 04 kv

--- R/pof_future_board_04kv.R
+++ R/pof_future_board_04kv.R
@@ -8,6 +8,7 @@
 #' @inheritParams pof_board_04kv
 #' @param simulation_end_year Numeric. The last year of simulating probability
 #'  of failure. Default is 100.
+#' @param gb_ref_given optional parameter to use custom reference values
 #' @return DataFrame. Future probability of failure
 #' along with future health score
 #' @export
@@ -48,7 +49,8 @@ pof_future_board_04kv <-
            k_value = 0.0069,
            c_value = 1.087,
            normal_expected_life = 60,
-           simulation_end_year = 100) {
+           simulation_end_year = 100,
+           gb_ref_given = NULL) {
 
     lv_asset_category <- "LV Board (WM)"
     `Asset Register Category` = `Health Index Asset Category` =
@@ -56,16 +58,23 @@ pof_future_board_04kv <-
       `K-Value (%)` = `C-Value` = `Asset Register  Category`  = NULL
     # due to NSE notes in R CMD check
 
-    asset_category <- gb_ref$categorisation_of_assets %>%
+    if(is.null(gb_ref_given)){
+      gb_ref_taken <- gb_ref
+    }else{
+      check_gb_ref_given(gb_ref_given)
+      gb_ref_taken <- gb_ref_given
+    }
+
+    asset_category <- gb_ref_taken$categorisation_of_assets %>%
       dplyr::filter(`Asset Register Category` ==
                       lv_asset_category) %>%
       dplyr::select(`Health Index Asset Category`) %>% dplyr::pull()
 
-    generic_term_1 <- gb_ref$generic_terms_for_assets %>%
+    generic_term_1 <- gb_ref_taken$generic_terms_for_assets %>%
       dplyr::filter(`Health Index Asset Category` == asset_category) %>%
       dplyr::select(`Generic Term...1`) %>% dplyr::pull()
 
-    generic_term_2 <- gb_ref$generic_terms_for_assets %>%
+    generic_term_2 <- gb_ref_taken$generic_terms_for_assets %>%
       dplyr::filter(`Health Index Asset Category` == asset_category) %>%
       dplyr::select(`Generic Term...2`) %>% dplyr::pull()
 
